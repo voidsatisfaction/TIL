6443e935bcdaa7bdb37f942117dba11b88f5e136
voidSatisfaction <lourie@naver.com>
2019-12-07 11:49:41 +0900

    algorithm: add solution of 11

--- Algorithm/[leetcode]nice_problems/disjoint_set/11_medium_[disjoint_set]_container_with_most_water.py
+++ Algorithm/[leetcode]nice_problems/disjoint_set/11_medium_[disjoint_set]_container_with_most_water.py
@@ -55,7 +55,21 @@ class Solution:
 
 class Solution:
   def maxArea(self, height: List[int]) -> int:
-    pass
+    maxArea, left, right = 0, 0, len(height)-1
+
+    while left < right:
+      minHeight = min(height[left], height[right])
+      maxArea = max(maxArea, minHeight * (right-left))
+
+      if height[left] < height[right]:
+        left += 1
+      elif height[left] > height[right]:
+        right -= 1
+      else:
+        left += 1
+        right -= 1
+
+    return maxArea
 
 print(Solution().maxArea([1,8,6,2,5,4,8,3,7]))
 print(Solution().maxArea([1,2,4,3]))
